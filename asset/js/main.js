0fd12e7f96c90d5f0de9a41dfae6c411f4551d42
0310_Natnaree Moeitom <moeitomnatnaree@gmail.com>
2026-03-02 18:05:10 +0700

    เพิ่มfunc. js(ยังไม่ขึ้นปฏิทิน)

--- asset/js/main.js
+++ asset/js/main.js
@@ -23,6 +23,7 @@ items.forEach((item, index) => {
 
 movecircle(0);
 
+// constหน้าแรก
 const monthTitle = document.getElementById("monthTitle")
 const calGrid = document.getElementById("calGrid");
 const summaryBox = document.getElementById("summarybox");
@@ -30,3 +31,25 @@ const summaryBox = document.getElementById("summarybox");
 const prevMonth = document.getAnimations("prevMonth");
 const nextMonth = document.getElementById("nextMonth");
 const btnAdd = document.getElementById("btnadd");
+
+//const page 2
+const btnBack = document.getElementById("btnBack");
+const btnAccept = document.getElementById("btnAccept");
+
+const pickedDateText = document.getElementById("pickedDateText");
+const bookingstartTime = document.getElementById("bookingstart");
+const bookingendTime = document.getElementById("bookingend");
+const note = document.getElementById("bookingnote");
+
+// หน้าbookingแรก
+let current = new Date(2026, 0 , 1);
+let selectDate = null; //เก็บวันที่เลือก
+let booking = []; //เก็บbooking {date,start,end,note}
+
+function pad2(n){
+  return String(n).padStart(2, "0"); //ถ้าวันที่เป็ฯหลักเดียวจะเติท0
+}
+function dateKey(d){
+  return `${d.getFullYear()}-${pad2(d.getMonth()+1)}-${pad2(d.getDate())}`;
+}//เก็บปี-เดือน-วันที่
+
